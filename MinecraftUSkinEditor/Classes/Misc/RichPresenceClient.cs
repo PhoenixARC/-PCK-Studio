4b26fcdfe11a0e758348a28df0bf28e95ba1014f
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-06-04 22:20:23 +0200

    Update RichPresence class

--- MinecraftUSkinEditor/Classes/Misc/RichPresenceClient.cs
+++ MinecraftUSkinEditor/Classes/Misc/RichPresenceClient.cs
@@ -7,38 +7,40 @@ namespace RichPresenceClient
     public static class RPC
     {
 
-        public static DiscordRpcClient client;
+        public static DiscordRpcClient client = null;
 
         public static void Initialize(string ClientID)
         {
-            //client = new DiscordRpcClient(ClientID);
-            //if (!client.Initialize())
-            //{
-            //    throw new Exception("ERROR initializing Discord RPC");
-            //}
+            client = new DiscordRpcClient(ClientID);
+            if (!client.Initialize())
+            {
+                throw new Exception("ERROR initializing Discord RPC");
+            }
         }
 
         public static void SetRPC(string details, string state, string imageLarge, string imageLargeText, string imageSmall)
         {
-            //if (client == null) return;
-            //client.SetPresence(new RichPresence()
-            //{
-            //    Details = details,
-            //    State = state,
-            //    Assets = new Assets()
-            //    {
-            //        LargeImageKey = imageLarge,
-            //        LargeImageText = imageLargeText,
-            //        SmallImageKey = imageSmall
-            //    }
-            //});
+            if (client == null) return;
+            client.SetPresence(new RichPresence()
+            {
+                Details = details,
+                State = state,
+                Assets = new Assets()
+                {
+                    LargeImageKey = imageLarge,
+                    LargeImageText = imageLargeText,
+                    SmallImageKey = imageSmall
+                }
+            });
         }
 
         public static void CloseRPC()
         {
-            //if (client != null)
-            //    client.Deinitialize();
-            //    client.Dispose();
+            if (client != null && client.IsInitialized)
+            {
+                client.Deinitialize();
+            }
+            client.Dispose();
         }
 
     }
