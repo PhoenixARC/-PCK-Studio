8586c2b88e4b92aedded2796736c5b703fbdb505
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-06-09 23:31:35 +0200

    Remove unnecessary includes in Entry point

--- MinecraftUSkinEditor/Classes/Program.cs
+++ MinecraftUSkinEditor/Classes/Program.cs
@@ -1,28 +1,20 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Threading;
 using System.Windows.Forms;
-using PckStudio.Classes;
 
 namespace PckStudio
 {
-
-
     static class Program
     {
         public static string baseurl = "http://api.pckstudio.xyz/api/pck";
         public static string backurl = "https://raw.githubusercontent.com/PhoenixARC/pckstudio.tk/main/studio/PCK/api/";
-        public static string Appdata = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/PCK Studio/";
+        public static string Appdata = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/PCK-Studio/";
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
-            Application.Run(new PckStudio.FormMain());
+            Application.Run(new FormMain());
         }
     }
 }
