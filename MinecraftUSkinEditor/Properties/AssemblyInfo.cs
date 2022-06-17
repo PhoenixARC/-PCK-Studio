a7548c2d4809bdfceea40eedd52eb7ebdd16111d
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-06-17 10:51:12 +0200

    Add description to AssemblyInfo

--- MinecraftUSkinEditor/Properties/AssemblyInfo.cs
+++ MinecraftUSkinEditor/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Security.Permissions;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PCK Studio")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Minecraft Legacy Console .pck Editor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nobledez & PhoenixARC")]
 [assembly: AssemblyProduct("PCK Studio")]
