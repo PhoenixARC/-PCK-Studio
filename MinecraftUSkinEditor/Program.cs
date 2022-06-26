d2f5403871d5441c08903a69629e2936821d8c0b
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-06-26 15:51:27 +0200

    Fix and moved Program.cs

--- MinecraftUSkinEditor/Program.cs
+++ MinecraftUSkinEditor/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace FUI_Studio
+namespace PckStudio
 {
     static class Program
     {
+        public static string baseurl = "http://api.pckstudio.xyz/api/pck";
+        public static string backurl = "https://raw.githubusercontent.com/PhoenixARC/pckstudio.tk/main/studio/PCK/api/";
+        public static string Appdata = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/PCK-Studio/";
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Forms.MainForm(args));
+            Application.Run(new MainForm());
         }
     }
 }
