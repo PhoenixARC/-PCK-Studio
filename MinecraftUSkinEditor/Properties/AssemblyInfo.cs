05606e1c3af54c9a018bf9b27490aedb780b50f9
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-07-29 20:31:58 +0200

    Set AssemblyVersion same as FileVersion and remove SecurityPermission

--- MinecraftUSkinEditor/Properties/AssemblyInfo.cs
+++ MinecraftUSkinEditor/Properties/AssemblyInfo.cs
@@ -15,7 +15,6 @@ using System.Security.Permissions;
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("PCK Studio")]
 [assembly: AssemblyCulture("")]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -34,6 +33,6 @@ using System.Security.Permissions;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.3.0.0")]
+[assembly: AssemblyVersion("6.4.0.0")]
 [assembly: AssemblyFileVersion("6.4.0.0")]
 [assembly: NeutralResourcesLanguage("")]
