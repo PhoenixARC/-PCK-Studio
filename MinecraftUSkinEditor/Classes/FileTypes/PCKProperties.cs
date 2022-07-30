c6e0b7cbdc837ba7dfbc8dac939ba6423d3c0099
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-07-30 21:45:28 +0200

    Add convenient functions to PCKProperties

--- MinecraftUSkinEditor/Classes/FileTypes/PCKProperties.cs
+++ MinecraftUSkinEditor/Classes/FileTypes/PCKProperties.cs
@@ -8,5 +8,25 @@ namespace PckStudio.Classes.FileTypes
 {
     public class PCKProperties : List<ValueTuple<string, string>> // class because `using` is file scoped :|
     {
+        public bool HasProperty(string property)
+        {
+            return GetProperty(property) != default;
+        }
+
+        public ValueTuple<string, string> GetProperty(string property)
+        {
+            return this.FirstOrDefault(p => p.Item1.Equals(property));
+        }
+
+        public void SetProperty(string property, string value)
+        {
+            if (HasProperty(property))
+            {
+                this[IndexOf(GetProperty(property))] = (property, value);
+                return;
+            }
+            Add((property, value));
+        }
+
     }
 }
