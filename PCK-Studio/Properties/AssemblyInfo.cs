0d3184c21c96473fc37abf51414b7f758f2bdbad
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-09-01 20:25:28 +0200

    Update AssemblyName from `PCK Studio` to `PCK-Studio`

--- PCK-Studio/Properties/AssemblyInfo.cs
+++ PCK-Studio/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Security.Permissions;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PCK Studio")]
+[assembly: AssemblyTitle("PCK-Studio")]
 [assembly: AssemblyDescription("A Minecraft Legacy Console .pck Editor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nobledez & PhoenixARC")]
