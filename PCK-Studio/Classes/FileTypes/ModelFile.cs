f14dd4fe4ea46f06aada670a811c0c4a8a6f0dff
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-10-15 14:03:13 +0200

    Add ModelReader and ModelFile class

---
+++ PCK-Studio/Classes/FileTypes/ModelFile.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace PckStudio.Classes.FileTypes
+{
+    [Serializable]
+    internal class ModelNotFoundException : Exception
+    {
+        public ModelNotFoundException()
+        {
+        }
+
+        public ModelNotFoundException(string message) : base(message)
+        {
+        }
+
+        public ModelNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ModelNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+    public class ModelFile
+    {
+        public List<Model> Models { get; } = new List<Model>();
+
+        public void AddModel(Model model)
+        {
+            Models.Add(model);
+        }
+
+        bool Contains(string name) => Models.FindIndex(m => m.name == name) > -1;
+
+        /// <exception cref="ModelNotFoundException"></exception>
+        Model GetModelByName(string name)
+        {
+            return Contains(name) ? Models.First(m => m.name.Equals(name)) : throw new ModelNotFoundException(nameof(name));
+        }
+
+        public struct Model
+        {
+            public readonly string name;
+            public Size textureSize;
+            public List<Part> parts { get; } = new List<Part>();
+
+            public Model(string name, int textureWidth, int textureHeight)
+            {
+                this.name = name;
+                textureSize = new Size(textureWidth, textureHeight);
+            }
+
+            public struct Part
+            {
+                string name;
+                (float x, float y, float z) position;
+                (float yaw, float pitch, float roll) rotation;
+                List<Box> Boxes { get; } = new List<Box>();
+
+                public struct Box
+                {
+                    (float x, float y, float z) Position;
+                    (int width, int height, int length) Size;
+                    float U, V;
+                    float Scale;
+                    bool Mirror;
+
+                    public Box((float x, float y, float z) position,
+                               (int width, int height, int length) size,
+                               float u, float v, float scale, bool mirror)
+                    {
+                        Position = position;
+                        Size = size;
+                        U = u;
+                        V = v;
+                        Scale = scale;
+                        Mirror = mirror;
+                    }
+                }
+
+                public Part(string name,
+                    (float x, float y, float z) pos,
+                    (float yaw, float pitch, float roll) rot) : this(name)
+                {
+                    position = pos;
+                    rotation = rot;
+                }
+
+                public Part(string name)
+                {
+                    this.name = name;
+                }
+
+                public void AddBox((float x, float y, float z) position,
+                            (int width, int height, int length) size,
+                            float u, float v, float scale, bool mirror)
+                {
+                    Boxes.Add(new Box(position, size, u, v, scale, mirror));
+                }
+
+            }
+
+            public void AddPart(Part part)
+            {
+                parts.Add(part);
+            }
+        }
+    }
+}
