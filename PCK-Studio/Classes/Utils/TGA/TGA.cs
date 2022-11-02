378e77db29a0b3df4f2249ba283de7e9b0b3e130
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-11-02 19:27:46 +0100

    Add copyright and resource links

--- PCK-Studio/Classes/Utils/TGA/TGA.cs
+++ PCK-Studio/Classes/Utils/TGA/TGA.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/* Copyright (c) 2022-present miku-666
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1.The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+**/
+using System;
 using System.IO;
 using System.Text;
 using System.Drawing;
@@ -9,12 +26,16 @@ using PckStudio.Classes.IO;
 
 namespace PckStudio.Classes.Utils.TGA
 {
+    /// <summary>
+    /// Resources:
+    ///  <http://www.paulbourke.net/dataformats/tga/>
+    ///  <https://en.wikipedia.org/wiki/Truevision_TGA#File_footer_(optional)>
+    /// </summary>
     internal static class TGA
     {
         private static TGAWriter _writer = new TGAWriter(true);
         private static TGAReader _reader = new TGAReader(true);
 
-        // http://www.paulbourke.net/dataformats/tga/
         private ref struct TGAHeader
         {
             public byte IdLength;
