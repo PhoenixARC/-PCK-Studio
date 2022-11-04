354dd0badc6e289cb997a201352425acf4725bfc
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-11-04 10:46:47 +0100

    Add TextPrompt form

---
+++ PCK-Studio/Forms/Additional-Popups/TextPrompt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PckStudio.Forms.Additional_Popups
+{
+    public partial class TextPrompt : MetroFramework.Forms.MetroForm
+    {
+        public string[] TextOutput => DialogResult == DialogResult.OK ? PromptTextBox.Lines : null;
+        public TextPrompt()
+        {
+            InitializeComponent();
+        }
+
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
