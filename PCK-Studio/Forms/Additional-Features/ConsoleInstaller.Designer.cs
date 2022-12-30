fc275a84f375208620a408df920edee7eca5a9d4
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-12-31 00:36:39 +0100

    Add Console Installer Form

---
+++ PCK-Studio/Forms/Additional-Features/ConsoleInstaller.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PckStudio.Forms.Additional_Features
+{
+    partial class ConsoleInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            MetroFramework.Controls.MetroLabel metroLabel1;
+            MetroFramework.Controls.MetroLabel metroLabel2;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConsoleInstaller));
+            this.selectedConsoleComboBox = new MetroFramework.Controls.MetroComboBox();
+            this.mainPanel = new System.Windows.Forms.TableLayoutPanel();
+            metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            metroLabel1.AutoSize = true;
+            metroLabel1.Location = new System.Drawing.Point(264, 73);
+            metroLabel1.Name = "metroLabel1";
+            metroLabel1.Size = new System.Drawing.Size(90, 19);
+            metroLabel1.Style = MetroFramework.MetroColorStyle.Black;
+            metroLabel1.TabIndex = 1;
+            metroLabel1.Text = "Console Type:";
+            metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroLabel2
+            // 
+            metroLabel2.AutoSize = true;
+            metroLabel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            metroLabel2.Location = new System.Drawing.Point(3, 0);
+            metroLabel2.Name = "metroLabel2";
+            metroLabel2.Size = new System.Drawing.Size(183, 35);
+            metroLabel2.Style = MetroFramework.MetroColorStyle.Black;
+            metroLabel2.TabIndex = 13;
+            metroLabel2.Text = "Console Type:";
+            metroLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // selectedConsoleComboBox
+            // 
+            this.selectedConsoleComboBox.BackColor = System.Drawing.SystemColors.Window;
+            this.selectedConsoleComboBox.FormattingEnabled = true;
+            this.selectedConsoleComboBox.ItemHeight = 23;
+            this.selectedConsoleComboBox.Items.AddRange(new object[] {
+            "Wii U",
+            "Play Station 3",
+            "PS Vita",
+            "Cemu",
+            "RPCS3"});
+            this.selectedConsoleComboBox.Location = new System.Drawing.Point(192, 3);
+            this.selectedConsoleComboBox.Name = "selectedConsoleComboBox";
+            this.selectedConsoleComboBox.PromptText = "Select console";
+            this.selectedConsoleComboBox.Size = new System.Drawing.Size(121, 29);
+            this.selectedConsoleComboBox.Style = MetroFramework.MetroColorStyle.Black;
+            this.selectedConsoleComboBox.TabIndex = 0;
+            this.selectedConsoleComboBox.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.selectedConsoleComboBox.UseSelectable = true;
+            this.selectedConsoleComboBox.SelectedIndexChanged += new System.EventHandler(this.selectedConsoleComboBox_SelectedIndexChanged);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.Transparent;
+            this.mainPanel.ColumnCount = 2;
+            this.mainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainPanel.Controls.Add(this.selectedConsoleComboBox, 1, 0);
+            this.mainPanel.Controls.Add(metroLabel2, 0, 0);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(20, 60);
+            this.mainPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.RowCount = 2;
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.mainPanel.Size = new System.Drawing.Size(379, 560);
+            this.mainPanel.TabIndex = 3;
+            // 
+            // ConsoleInstaller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 640);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(metroLabel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(419, 640);
+            this.Name = "ConsoleInstaller";
+            this.Style = MetroFramework.MetroColorStyle.Silver;
+            this.Text = "Console Installer";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroComboBox selectedConsoleComboBox;
+        private System.Windows.Forms.TableLayoutPanel mainPanel;
+    }
+}
---
+++ PCK-Studio/Forms/Additional-Features/ConsoleInstaller.cs
@@ -0,0 +1,56 @@
+﻿/* Copyright (c) 2022-present miku-666
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+**/
+using System;
+using System.Windows.Forms;
+namespace PckStudio.Forms.Additional_Features
+{
+    public partial class ConsoleInstaller : MetroFramework.Forms.MetroForm
+    {
+        private Control shownControl;
+
+        public ConsoleInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void selectedConsoleComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Remove previous shown Control
+            mainPanel.Controls.Remove(shownControl);
+            if (selectedConsoleComboBox.SelectedIndex > -1)
+            {
+                string text = selectedConsoleComboBox.Items[selectedConsoleComboBox.SelectedIndex].ToString();
+                shownControl = text switch
+                {
+                    "Wii U" => new WiiUInstallPanel(),
+                    "Play Station 3" => new PS3InstallPanel(),
+                    "PS Vita" => throw new NotImplementedException($"{text}-Installer is currently not implemented."),
+                    "Cemu" => throw new NotImplementedException($"{text}-Installer is currently not implemented."),
+                    "RPCS3" => throw new NotImplementedException($"{text}-Installer is currently not implemented."),
+                    _ => throw new Exception($"No Control found for: {text}"),
+                };
+
+                shownControl.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Bottom | AnchorStyles.Right;
+                
+
+                mainPanel.SetColumnSpan(shownControl, 2);
+                mainPanel.Controls.Add(shownControl, 0, 1);
+            }
+        }
+    }
+}
