21ec9e25f2e74a43a3203a99bff83876b8ef762c
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-03-04 14:12:28 +0100

    Update ImageExtention to support different width and height sub images

--- PCK-Studio/Classes/Extentions/ImageExtentions.cs
+++ PCK-Studio/Classes/Extentions/ImageExtentions.cs
@@ -7,28 +7,34 @@ namespace PckStudio.Classes.Extentions
 {
 	internal static class ImageExtentions
 	{
-        public static IEnumerable<Image> CreateImageList(this Image source, int size)
+        public static IEnumerable<Image> CreateImageList(this Image source, Size size)
         {
-            int img_row_count = source.Width / size;
-            int img_column_count = source.Height / size;
-            Debug.WriteLine($"{source.Width} {source.Height} {size} {size} {img_column_count} {img_row_count}");
+            int img_row_count = source.Width / size.Width;
+            int img_column_count = source.Height / size.Height;
+            Debug.WriteLine($"{source.Width} {source.Height} {size} {img_column_count} {img_row_count}");
             for (int i = 0; i < img_column_count * img_row_count; i++)
             {
                 int row = i / img_row_count;
                 int column = i % img_row_count;
-                Rectangle tileArea = new Rectangle(new Point(column * size, row * size), new Size(size, size));
-                Bitmap tileImage = new Bitmap(size, size);
+                Rectangle tileArea = new Rectangle(new Point(column * size.Height, row * size.Width), size);
+                Bitmap tileImage = new Bitmap(size.Width, size.Height);
                 using (Graphics gfx = Graphics.FromImage(tileImage))
                 {
                     gfx.SmoothingMode = SmoothingMode.None;
                     gfx.InterpolationMode = InterpolationMode.NearestNeighbor;
                     gfx.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
-                    gfx.DrawImage(source, new Rectangle(0, 0, size, size), tileArea, GraphicsUnit.Pixel);
+                    gfx.DrawImage(source, new Rectangle(Point.Empty, size), tileArea, GraphicsUnit.Pixel);
                 }
                 yield return tileImage;
             }
             yield break;
+        }
+
+
+        public static IEnumerable<Image> CreateImageList(this Image source, int scalar)
+        {
+            return CreateImageList(source, new Size(scalar, scalar));
         }
     }
 }
