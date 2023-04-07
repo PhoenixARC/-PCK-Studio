476b4755ee0e54f0c1e04f5a71e0bb5ee7e28635
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-04-07 17:59:53 +0200

    AboutThisProgram.cs - Made setting developer icons and names thread safe

---
+++ PCK-Studio/Classes/Extentions/ControlExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PckStudio.Classes.Extentions
+{
+    internal static class ControlExtensions
+    {
+        private delegate void SetPropertyThreadSafeDelegate<TResult>(
+                Control @this,
+                Expression<Func<TResult>> property,
+                TResult value);
+
+        public static void SetPropertyThreadSafe<TResult>(
+            this Control @this,
+            Expression<Func<TResult>> property,
+            TResult value)
+        {
+            var propertyInfo = (property.Body as MemberExpression).Member
+                as PropertyInfo;
+
+            if (propertyInfo == null ||
+                !@this.GetType().IsSubclassOf(propertyInfo.ReflectedType) ||
+                @this.GetType().GetProperty(
+                    propertyInfo.Name,
+                    propertyInfo.PropertyType) == null)
+            {
+                throw new ArgumentException("The lambda expression 'property' must reference a valid property on this Control.");
+            }
+
+            if (@this.InvokeRequired)
+            {
+                @this.Invoke(new SetPropertyThreadSafeDelegate<TResult>(SetPropertyThreadSafe), @this, property, value);
+            }
+            else
+            {
+                @this.GetType().InvokeMember(
+                    propertyInfo.Name,
+                    BindingFlags.SetProperty,
+                    null,
+                    @this,
+                    new object[] { value });
+            }
+        }
+    }
+}
--- PCK-Studio/Forms/Additional-Popups/AboutThisProgram.cs
+++ PCK-Studio/Forms/Additional-Popups/AboutThisProgram.cs
@@ -38,16 +38,20 @@ namespace PckStudio
                 // TODO: check if avatar has changed and only acquire info once
                 var devs = await AcquireDeveloperUserInfoAsync("PhoenixARC", "MattN-L", "EternalModz", "NessieHax");
 
-                phoenixarcPictureBox.Image = ImageExtentions.ImageFromUrl(devs[0].AvatarUrl);
+                phoenixarcPictureBox.SetPropertyThreadSafe(() => phoenixarcPictureBox.Image, ImageExtentions.ImageFromUrl(devs[0].AvatarUrl));
+                phoenixarcPictureBox.SetPropertyThreadSafe(() => phoenixarcPictureBox.Text, devs[0].Name);
                 phoenixarcGitHubButton.Click += (sender, e) => Process.Start(devs[0].HtmlUrl);
 
-                mattNLPictureBox.Image = ImageExtentions.ImageFromUrl(devs[1].AvatarUrl);
+                mattNLPictureBox.SetPropertyThreadSafe(() => mattNLPictureBox.Image, ImageExtentions.ImageFromUrl(devs[1].AvatarUrl));
+                mattNLPictureBox.SetPropertyThreadSafe(() => mattNLPictureBox.Text, devs[1].Name);
                 mattNLGitHubButton.Click += (sender, e) => Process.Start(devs[1].HtmlUrl);
 
-                eternalModzPictureBox.Image = ImageExtentions.ImageFromUrl(devs[2].AvatarUrl);
+                eternalModzPictureBox.SetPropertyThreadSafe(() => eternalModzPictureBox.Image, ImageExtentions.ImageFromUrl(devs[2].AvatarUrl));
+                eternalModzPictureBox.SetPropertyThreadSafe(() => eternalModzPictureBox.Text, devs[2].Name);
                 eternalModzGitHubButton.Click += (sender, e) => Process.Start(devs[2].HtmlUrl);
 
-                mikuPictureBox.Image = ImageExtentions.ImageFromUrl(devs[3].AvatarUrl);
+                mikuPictureBox.SetPropertyThreadSafe(() => mikuPictureBox.Image, ImageExtentions.ImageFromUrl(devs[3].AvatarUrl));
+                mikuPictureBox.SetPropertyThreadSafe(() => mikuLabel.Text, devs[3].Name);
                 mikuGitHubButton.Click += (sender, e) => Process.Start(devs[3].HtmlUrl);
             });
         }
