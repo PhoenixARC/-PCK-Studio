910d95c55607c553da1b18ae9175e26b0512d50c
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-04-17 17:49:59 +0200

    WiiUPanel.cs - Renamed some designer component names

--- PCK-Studio/Forms/Features/WiiUPanel.Designer.cs
+++ PCK-Studio/Forms/Features/WiiUPanel.Designer.cs
@@ -79,13 +79,13 @@
             this.myTablePanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.myTablePanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.myTablePanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.myTablePanel1.Size = new System.Drawing.Size(518, 616);
+            this.myTablePanel1.Size = new System.Drawing.Size(430, 550);
             this.myTablePanel1.TabIndex = 3;
             // 
             // buttonServerToggle
             // 
             this.buttonServerToggle.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.buttonServerToggle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(178)))), ((int)(((byte)(13)))));
+            this.buttonServerToggle.BackColor = System.Drawing.Color.SpringGreen;
             this.buttonServerToggle.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonServerToggle.Enabled = false;
             this.buttonServerToggle.FlatAppearance.BorderSize = 0;
@@ -93,15 +93,15 @@
             this.buttonServerToggle.Font = new System.Drawing.Font("Segoe UI", 9.75F);
             this.buttonServerToggle.ForeColor = System.Drawing.Color.White;
             this.buttonServerToggle.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.buttonServerToggle.Location = new System.Drawing.Point(347, 3);
+            this.buttonServerToggle.Location = new System.Drawing.Point(289, 3);
             this.buttonServerToggle.Name = "buttonServerToggle";
-            this.buttonServerToggle.Size = new System.Drawing.Size(168, 27);
+            this.buttonServerToggle.Size = new System.Drawing.Size(138, 27);
             this.buttonServerToggle.TabIndex = 9;
             this.buttonServerToggle.Text = "Start";
             this.buttonServerToggle.UseVisualStyleBackColor = false;
             this.buttonServerToggle.Click += new System.EventHandler(this.buttonServerToggle_Click);
             // 
-            // textBoxHost
+            // IPv4TextBox
             // 
             this.IPv4TextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.myTablePanel1.SetColumnSpan(this.IPv4TextBox, 2);
@@ -123,7 +123,7 @@
             this.IPv4TextBox.Lines = new string[0];
             this.IPv4TextBox.Location = new System.Drawing.Point(3, 3);
             this.IPv4TextBox.MaxLength = 32767;
-            this.IPv4TextBox.Name = "textBoxHost";
+            this.IPv4TextBox.Name = "IPv4TextBox";
             this.IPv4TextBox.PasswordChar = '\0';
             this.IPv4TextBox.PromptText = "Wii U IP";
             this.IPv4TextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
@@ -131,7 +131,7 @@
             this.IPv4TextBox.SelectionLength = 0;
             this.IPv4TextBox.SelectionStart = 0;
             this.IPv4TextBox.ShortcutsEnabled = true;
-            this.IPv4TextBox.Size = new System.Drawing.Size(338, 27);
+            this.IPv4TextBox.Size = new System.Drawing.Size(280, 27);
             this.IPv4TextBox.Style = MetroFramework.MetroColorStyle.Blue;
             this.IPv4TextBox.TabIndex = 10;
             this.IPv4TextBox.Theme = MetroFramework.MetroThemeStyle.Dark;
@@ -150,7 +150,7 @@
             this.listViewPCKS.HideSelection = false;
             this.listViewPCKS.Location = new System.Drawing.Point(3, 151);
             this.listViewPCKS.Name = "listViewPCKS";
-            this.listViewPCKS.Size = new System.Drawing.Size(512, 462);
+            this.listViewPCKS.Size = new System.Drawing.Size(424, 396);
             this.listViewPCKS.TabIndex = 3;
             this.listViewPCKS.UseCompatibleStateImageBehavior = false;
             this.listViewPCKS.View = System.Windows.Forms.View.Details;
@@ -162,7 +162,7 @@
             this.replaceToolStripMenuItem,
             this.replacePCKToolStripMenuItem});
             this.contextMenuStripCaffiine.Name = "contextMenuStripCaffiine";
-            this.contextMenuStripCaffiine.Size = new System.Drawing.Size(212, 70);
+            this.contextMenuStripCaffiine.Size = new System.Drawing.Size(212, 48);
             // 
             // replaceToolStripMenuItem
             // 
@@ -194,9 +194,9 @@
             this.radioButtonUSB.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.radioButtonUSB.ForeColor = System.Drawing.Color.White;
             this.radioButtonUSB.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.radioButtonUSB.Location = new System.Drawing.Point(347, 69);
+            this.radioButtonUSB.Location = new System.Drawing.Point(289, 69);
             this.radioButtonUSB.Name = "radioButtonUSB";
-            this.radioButtonUSB.Size = new System.Drawing.Size(168, 30);
+            this.radioButtonUSB.Size = new System.Drawing.Size(138, 30);
             this.radioButtonUSB.TabIndex = 6;
             this.radioButtonUSB.Text = "USB";
             this.radioButtonUSB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -228,12 +228,13 @@
             this.TextBoxPackImage.Name = "TextBoxPackImage";
             this.TextBoxPackImage.PasswordChar = '\0';
             this.TextBoxPackImage.PromptText = "Pack Image";
+            this.TextBoxPackImage.ReadOnly = true;
             this.TextBoxPackImage.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.TextBoxPackImage.SelectedText = "";
             this.TextBoxPackImage.SelectionLength = 0;
             this.TextBoxPackImage.SelectionStart = 0;
             this.TextBoxPackImage.ShortcutsEnabled = true;
-            this.TextBoxPackImage.Size = new System.Drawing.Size(338, 27);
+            this.TextBoxPackImage.Size = new System.Drawing.Size(280, 27);
             this.TextBoxPackImage.Style = MetroFramework.MetroColorStyle.Blue;
             this.TextBoxPackImage.TabIndex = 11;
             this.TextBoxPackImage.Theme = MetroFramework.MetroThemeStyle.Dark;
@@ -249,16 +250,16 @@
             this.radioButtonSystem.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.radioButtonSystem.Checked = true;
             this.radioButtonSystem.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.radioButtonSystem.FlatAppearance.CheckedBackColor = System.Drawing.Color.Teal;
+            this.radioButtonSystem.FlatAppearance.CheckedBackColor = System.Drawing.Color.DodgerBlue;
             this.radioButtonSystem.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua;
             this.radioButtonSystem.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.radioButtonSystem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.radioButtonSystem.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.radioButtonSystem.ForeColor = System.Drawing.Color.White;
             this.radioButtonSystem.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.radioButtonSystem.Location = new System.Drawing.Point(175, 69);
+            this.radioButtonSystem.Location = new System.Drawing.Point(146, 69);
             this.radioButtonSystem.Name = "radioButtonSystem";
-            this.radioButtonSystem.Size = new System.Drawing.Size(166, 30);
+            this.radioButtonSystem.Size = new System.Drawing.Size(137, 30);
             this.radioButtonSystem.TabIndex = 5;
             this.radioButtonSystem.TabStop = true;
             this.radioButtonSystem.Text = "System";
@@ -277,7 +278,7 @@
             this.buttonSelect.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.buttonSelect.Location = new System.Drawing.Point(3, 69);
             this.buttonSelect.Name = "buttonSelect";
-            this.buttonSelect.Size = new System.Drawing.Size(166, 30);
+            this.buttonSelect.Size = new System.Drawing.Size(137, 30);
             this.buttonSelect.TabIndex = 1;
             this.buttonSelect.UseVisualStyleBackColor = true;
             this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
@@ -285,7 +286,7 @@
             // PackImageSelection
             // 
             this.PackImageSelection.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.PackImageSelection.BackColor = System.Drawing.Color.Sienna;
+            this.PackImageSelection.BackColor = System.Drawing.Color.DarkCyan;
             this.PackImageSelection.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PackImageSelection.Enabled = false;
             this.PackImageSelection.FlatAppearance.BorderSize = 0;
@@ -293,9 +294,9 @@
             this.PackImageSelection.Font = new System.Drawing.Font("Segoe UI", 9.75F);
             this.PackImageSelection.ForeColor = System.Drawing.Color.White;
             this.PackImageSelection.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.PackImageSelection.Location = new System.Drawing.Point(347, 36);
+            this.PackImageSelection.Location = new System.Drawing.Point(289, 36);
             this.PackImageSelection.Name = "PackImageSelection";
-            this.PackImageSelection.Size = new System.Drawing.Size(168, 27);
+            this.PackImageSelection.Size = new System.Drawing.Size(138, 27);
             this.PackImageSelection.TabIndex = 12;
             this.PackImageSelection.Text = "Browse";
             this.PackImageSelection.UseVisualStyleBackColor = false;
@@ -316,7 +317,7 @@
             this.regionLayoutPanel.Name = "regionLayoutPanel";
             this.regionLayoutPanel.RowCount = 1;
             this.regionLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.regionLayoutPanel.Size = new System.Drawing.Size(512, 40);
+            this.regionLayoutPanel.Size = new System.Drawing.Size(424, 40);
             this.regionLayoutPanel.TabIndex = 13;
             // 
             // radioButtonEur
@@ -327,7 +328,7 @@
             this.radioButtonEur.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.radioButtonEur.Checked = true;
             this.radioButtonEur.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.radioButtonEur.FlatAppearance.CheckedBackColor = System.Drawing.Color.Teal;
+            this.radioButtonEur.FlatAppearance.CheckedBackColor = System.Drawing.Color.DodgerBlue;
             this.radioButtonEur.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua;
             this.radioButtonEur.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.radioButtonEur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -336,7 +337,7 @@
             this.radioButtonEur.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.radioButtonEur.Location = new System.Drawing.Point(3, 3);
             this.radioButtonEur.Name = "radioButtonEur";
-            this.radioButtonEur.Size = new System.Drawing.Size(164, 34);
+            this.radioButtonEur.Size = new System.Drawing.Size(135, 34);
             this.radioButtonEur.TabIndex = 1;
             this.radioButtonEur.TabStop = true;
             this.radioButtonEur.Text = "EUR";
@@ -358,9 +359,9 @@
             this.radioButtonUs.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.radioButtonUs.ForeColor = System.Drawing.Color.White;
             this.radioButtonUs.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.radioButtonUs.Location = new System.Drawing.Point(173, 3);
+            this.radioButtonUs.Location = new System.Drawing.Point(144, 3);
             this.radioButtonUs.Name = "radioButtonUs";
-            this.radioButtonUs.Size = new System.Drawing.Size(164, 34);
+            this.radioButtonUs.Size = new System.Drawing.Size(135, 34);
             this.radioButtonUs.TabIndex = 0;
             this.radioButtonUs.Text = "US";
             this.radioButtonUs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -381,23 +382,23 @@
             this.radioButtonJap.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.radioButtonJap.ForeColor = System.Drawing.Color.White;
             this.radioButtonJap.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.radioButtonJap.Location = new System.Drawing.Point(343, 3);
+            this.radioButtonJap.Location = new System.Drawing.Point(285, 3);
             this.radioButtonJap.Name = "radioButtonJap";
-            this.radioButtonJap.Size = new System.Drawing.Size(166, 34);
+            this.radioButtonJap.Size = new System.Drawing.Size(136, 34);
             this.radioButtonJap.TabIndex = 2;
             this.radioButtonJap.Text = "JAP";
             this.radioButtonJap.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.radioButtonJap.UseVisualStyleBackColor = false;
             this.radioButtonJap.Click += new System.EventHandler(this.radioButton_Click);
             // 
-            // WiiUInstallPanel
+            // WiiUPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Controls.Add(this.myTablePanel1);
-            this.Name = "WiiUInstallPanel";
-            this.Size = new System.Drawing.Size(518, 616);
+            this.Name = "WiiUPanel";
+            this.Size = new System.Drawing.Size(430, 550);
             this.myTablePanel1.ResumeLayout(false);
             this.contextMenuStripCaffiine.ResumeLayout(false);
             this.regionLayoutPanel.ResumeLayout(false);
