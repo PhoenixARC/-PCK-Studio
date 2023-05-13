ce41a6c355caf33dfddef21135e0e6cbd5ef03f0
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-05-13 19:00:34 +0200

    Splited ImageLayoutDirection and ImageSection into their own files

--- PCK-Studio/Extensions/ImageExtensions.cs
+++ PCK-Studio/Extensions/ImageExtensions.cs
@@ -9,47 +9,8 @@ using System.Linq;
 
 namespace PckStudio.Extensions
 {
-    internal enum ImageLayoutDirection
-    {
-        Horizontal,
-        Vertical
-    }
-
     internal static class ImageExtensions
     {
-        private struct ImageSection
-        {
-            public readonly Size Size;
-            public readonly Point Point;
-            public readonly Rectangle Area;
-
-            internal ImageSection(Size sectionSize, int index, ImageLayoutDirection layoutDirection)
-            {
-                switch(layoutDirection)
-                {
-                    case ImageLayoutDirection.Horizontal:
-                        {
-                            Size = new Size(sectionSize.Height, sectionSize.Height);
-                            Point = new Point(index * sectionSize.Height, 0);
-                        }
-                        break;
-
-                    case ImageLayoutDirection.Vertical:
-                        {
-                            Size = new Size(sectionSize.Width, sectionSize.Width);
-                            Point = new Point(0, index * sectionSize.Width);
-                        }
-                        break;
-
-                    default:
-                        Size = Size.Empty;
-                        Point = new Point(-1, -1);
-                        break;
-                }
-                Area = new Rectangle(Point, Size);
-            }
-        }
-
         internal static Image GetArea(this Image source, Rectangle area)
         {
             Image tileImage = new Bitmap(area.Width, area.Height);
