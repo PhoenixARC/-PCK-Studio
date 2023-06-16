f1db177533be016a9867e80d6a9dcfa961cf2d0b
MattNL <MattNL.XBLD@gmail.com>
2023-06-16 08:47:25 -0400

    Added save icon to resources

--- PCK-Studio/Properties/Resources.Designer.cs
+++ PCK-Studio/Properties/Resources.Designer.cs
@@ -537,6 +537,16 @@ namespace PckStudio.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap SaveIcon {
+            get {
+                object obj = ResourceManager.GetObject("SaveIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap SKIN_ICON {
             get {
                 object obj = ResourceManager.GetObject("SKIN_ICON", resourceCulture);
