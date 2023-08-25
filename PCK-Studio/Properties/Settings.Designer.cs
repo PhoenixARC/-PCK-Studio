143984a5d67961eb9b5a69d3dd22662f74a9d718
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-08-25 12:50:19 +0200

    Added setting to use a combo box when setting parameter name inside the grf editor

--- PCK-Studio/Properties/Settings.Designer.cs
+++ PCK-Studio/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PckStudio.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -58,7 +58,7 @@ namespace PckStudio.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool LoadSubPcks {
             get {
                 return ((bool)(this["LoadSubPcks"]));
@@ -77,6 +77,18 @@ namespace PckStudio.Properties {
             }
             set {
                 this["ShowRichPresence"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool UseComboBoxForGRFParameter {
+            get {
+                return ((bool)(this["UseComboBoxForGRFParameter"]));
+            }
+            set {
+                this["UseComboBoxForGRFParameter"] = value;
             }
         }
     }
