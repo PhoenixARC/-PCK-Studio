8817ae9fc1011e569c66135bf1d4c3a863cd53ba
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-08-25 12:57:14 +0200

    AnimationEditor - Added Copyright notice

--- PCK-Studio/Forms/Editor/AnimationEditor.cs
+++ PCK-Studio/Forms/Editor/AnimationEditor.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/* Copyright (c) 2023-present miku-666, MattNL
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1.The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+**/
+using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -10,8 +27,6 @@ using MetroFramework.Forms;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-
-using OMI.Formats.Pck;
 
 using PckStudio.Forms.Additional_Popups.Animation;
 using PckStudio.Extensions;
