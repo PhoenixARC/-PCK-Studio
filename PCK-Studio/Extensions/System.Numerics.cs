a90a98f8bc60c76770e869cc7a1a075a3c8de40b
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-11-01 10:20:31 +0100

    NumericsExtensions - Remove unnecessary using statements

--- PCK-Studio/Extensions/System.Numerics.cs
+++ PCK-Studio/Extensions/System.Numerics.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PckStudio.Extensions
+﻿namespace PckStudio.Extensions
 {
     internal static class NumericsExtensions
     {
