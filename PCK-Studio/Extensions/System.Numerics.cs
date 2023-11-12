38d317152ef9737bf195b95312b37492d87b6f0e
miku-666 <74728189+NessieHax@users.noreply.github.com>
2023-11-12 12:04:55 +0100

    NumericsExtensions - Changed parameter name

--- PCK-Studio/Extensions/System.Numerics.cs
+++ PCK-Studio/Extensions/System.Numerics.cs
@@ -7,9 +7,9 @@
             return new OpenTK.Vector3(vector3.X, vector3.Y, vector3.Z);
         }
         
-        internal static OpenTK.Vector2 ToOpenTKVector(this System.Numerics.Vector2 vector3)
+        internal static OpenTK.Vector2 ToOpenTKVector(this System.Numerics.Vector2 vector2)
         {
-            return new OpenTK.Vector2(vector3.X, vector3.Y);
+            return new OpenTK.Vector2(vector2.X, vector2.Y);
         }
     }
 }
