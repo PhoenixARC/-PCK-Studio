1aa78696b0764bead9edcf37aad26aca32cb3099
miku-666 <74728189+NessieHax@users.noreply.github.com>
2024-02-07 16:54:26 +0100

    Add Copyright header to rendering classes

--- PCK-Studio/Rendering/Renderer.cs
+++ PCK-Studio/Rendering/Renderer.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright (c) 2023-present miku-666
+﻿/* Copyright (c) 2024-present miku-666
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
