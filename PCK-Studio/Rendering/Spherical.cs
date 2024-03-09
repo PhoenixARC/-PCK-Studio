a80780bf97d4bbf433100ef515445fdfc43ab018
miku-666 <74728189+NessieHax@users.noreply.github.com>
2024-03-09 22:23:53 +0100

    Spherical.cs - Remove GetPosition function

--- PCK-Studio/Rendering/Spherical.cs
+++ PCK-Studio/Rendering/Spherical.cs
@@ -43,23 +43,9 @@ namespace PckStudio.Rendering
             set => vector.Z = value;
         }
 
-        public Vector3 GetPosition()
-        {
-            float sineAzimuthal   = (float)Math.Sin(MathHelper.DegreesToRadians(Phi));
-            float cosineAzimuthal = (float)Math.Cos(MathHelper.DegreesToRadians(Phi));
-            float sinePolar       = (float)Math.Sin(MathHelper.DegreesToRadians(Theta));
-            float cosinePolar     = (float)Math.Cos(MathHelper.DegreesToRadians(Theta));
-
-            Vector3 direction = new Vector3();
-            direction.X = cosinePolar * sineAzimuthal;
-            direction.Y = sineAzimuthal;
-            direction.Z = sinePolar * cosineAzimuthal;
-            return direction * Radius;
-        }
-
         public override string ToString()
         {
-            return $"Radius: {Radius}; Theta: {Theta}; Phi: {Phi}; Position: {GetPosition()}";
+            return $"Radius: {Radius}; Theta: {Theta}; Phi: {Phi};";
         }
     }
 }
