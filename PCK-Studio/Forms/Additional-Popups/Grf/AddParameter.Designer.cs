243c3fda0e87983da47aff63f4d20dc244e146ad
miku-666 <74728189+NessieHax@users.noreply.github.com>
2024-07-20 13:20:52 +0200

    GRF/AddParameter - Add auto complete to param promt and remove combobox option

--- PCK-Studio/Forms/Additional-Popups/Grf/AddParameter.Designer.cs
+++ PCK-Studio/Forms/Additional-Popups/Grf/AddParameter.Designer.cs
@@ -35,7 +35,6 @@
             this.ValueTextBox = new MetroFramework.Controls.MetroTextBox();
             this.CancelBtn = new MetroFramework.Controls.MetroButton();
             this.ConfirmBtn = new MetroFramework.Controls.MetroButton();
-            this.availableComboBox = new MetroFramework.Controls.MetroComboBox();
             metroLabel1 = new MetroFramework.Controls.MetroLabel();
             metroLabel2 = new MetroFramework.Controls.MetroLabel();
             this.SuspendLayout();
@@ -46,7 +45,7 @@
             metroLabel1.Location = new System.Drawing.Point(18, 27);
             metroLabel1.Name = "metroLabel1";
             metroLabel1.Size = new System.Drawing.Size(48, 19);
-            metroLabel1.TabIndex = 0;
+            metroLabel1.TabIndex = 4;
             metroLabel1.Text = "Name:";
             metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
             // 
@@ -56,12 +55,14 @@
             metroLabel2.Location = new System.Drawing.Point(17, 56);
             metroLabel2.Name = "metroLabel2";
             metroLabel2.Size = new System.Drawing.Size(42, 19);
-            metroLabel2.TabIndex = 1;
+            metroLabel2.TabIndex = 5;
             metroLabel2.Text = "Value:";
             metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
             // 
             // NameTextBox
             // 
+            this.NameTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.NameTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             // 
             // 
             // 
@@ -86,7 +87,7 @@
             this.NameTextBox.ShortcutsEnabled = true;
             this.NameTextBox.Size = new System.Drawing.Size(165, 23);
             this.NameTextBox.Style = MetroFramework.MetroColorStyle.White;
-            this.NameTextBox.TabIndex = 2;
+            this.NameTextBox.TabIndex = 0;
             this.NameTextBox.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.NameTextBox.UseSelectable = true;
             this.NameTextBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -117,8 +118,7 @@
             this.ValueTextBox.SelectionStart = 0;
             this.ValueTextBox.ShortcutsEnabled = true;
             this.ValueTextBox.Size = new System.Drawing.Size(165, 23);
-            this.ValueTextBox.Style = MetroFramework.MetroColorStyle.White;
-            this.ValueTextBox.TabIndex = 3;
+            this.ValueTextBox.TabIndex = 1;
             this.ValueTextBox.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.ValueTextBox.UseSelectable = true;
             this.ValueTextBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -131,7 +131,7 @@
             this.CancelBtn.Name = "CancelBtn";
             this.CancelBtn.Size = new System.Drawing.Size(95, 23);
             this.CancelBtn.Style = MetroFramework.MetroColorStyle.White;
-            this.CancelBtn.TabIndex = 4;
+            this.CancelBtn.TabIndex = 2;
             this.CancelBtn.Text = "Cancel";
             this.CancelBtn.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.CancelBtn.UseSelectable = true;
@@ -142,25 +142,11 @@
             this.ConfirmBtn.Name = "ConfirmBtn";
             this.ConfirmBtn.Size = new System.Drawing.Size(96, 23);
             this.ConfirmBtn.Style = MetroFramework.MetroColorStyle.White;
-            this.ConfirmBtn.TabIndex = 5;
+            this.ConfirmBtn.TabIndex = 3;
             this.ConfirmBtn.Text = "Confirm";
             this.ConfirmBtn.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.ConfirmBtn.UseSelectable = true;
             this.ConfirmBtn.Click += new System.EventHandler(this.ConfirmButton_Click);
-            // 
-            // availableComboBox
-            // 
-            this.availableComboBox.FormattingEnabled = true;
-            this.availableComboBox.ItemHeight = 23;
-            this.availableComboBox.Location = new System.Drawing.Point(72, 21);
-            this.availableComboBox.Name = "availableComboBox";
-            this.availableComboBox.Size = new System.Drawing.Size(165, 29);
-            this.availableComboBox.Style = MetroFramework.MetroColorStyle.Silver;
-            this.availableComboBox.TabIndex = 6;
-            this.availableComboBox.Theme = MetroFramework.MetroThemeStyle.Dark;
-            this.availableComboBox.UseSelectable = true;
-            this.availableComboBox.Visible = false;
-            this.availableComboBox.SelectedIndexChanged += new System.EventHandler(this.availableComboBox_SelectedIndexChanged);
             // 
             // AddParameter
             // 
@@ -168,7 +154,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(264, 126);
-            this.Controls.Add(this.availableComboBox);
             this.Controls.Add(this.ConfirmBtn);
             this.Controls.Add(this.CancelBtn);
             this.Controls.Add(this.ValueTextBox);
@@ -192,6 +177,5 @@
         private MetroFramework.Controls.MetroButton CancelBtn;
         private MetroFramework.Controls.MetroButton ConfirmBtn;
         private MetroFramework.Controls.MetroTextBox NameTextBox;
-        private MetroFramework.Controls.MetroComboBox availableComboBox;
     }
 }
