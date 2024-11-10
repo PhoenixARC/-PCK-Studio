b4ca5ad81027a8141d66fbfc1098f70842dd1ad5
miku-666 <74728189+NessieHax@users.noreply.github.com>
2024-11-10 19:28:19 +0100

    ITryGetSet - mark classes and interfaces public

--- PCK-Studio/Interfaces/ITryGetSet.cs
+++ PCK-Studio/Interfaces/ITryGetSet.cs
@@ -3,7 +3,7 @@
     public delegate bool TryGetDelegate<in TKey, TValue>(TKey key, out TValue value);
     public delegate bool TrySetDelegate<in TKey, TValue>(TKey key, TValue value);
 
-    internal sealed class TryGet<TKey, TValue> : ITryGet<TKey, TValue>
+    public sealed class TryGet<TKey, TValue> : ITryGet<TKey, TValue>
     {
         private TryGetDelegate<TKey, TValue> _tryGetDelegate;
 
@@ -17,7 +17,7 @@
         }
     }
 
-    internal sealed class TrySet<TKey, TValue> : ITrySet<TKey, TValue>
+    public sealed class TrySet<TKey, TValue> : ITrySet<TKey, TValue>
     {
         private TrySetDelegate<TKey, TValue> _trySetDelegate;
 
@@ -31,7 +31,7 @@
         }
     }
 
-    internal sealed class TryGetSet<TKey, TValue> : ITryGetSet<TKey, TValue>
+    public sealed class TryGetSet<TKey, TValue> : ITryGetSet<TKey, TValue>
     {
         public static ITryGetSet<TKey, TValue> FromDelegates(TryGetDelegate<TKey, TValue> tryGetDelegate, TrySetDelegate<TKey, TValue> trySetDelegate) => new TryGetSet<TKey, TValue>(tryGetDelegate, trySetDelegate);
 
@@ -49,17 +49,17 @@
         }
     }
 
-    interface ITryGet<in TKey, TValue>
+    public interface ITryGet<in TKey, TValue>
     {
         bool TryGet(TKey key, out TValue value);
-    }    
-    
-    interface ITrySet<in TKey, TValue>
+    }
+
+    public interface ITrySet<in TKey, TValue>
     {
         bool TrySet(TKey key, TValue value);
     }
 
-    interface ITryGetSet<in TKey, TValue> : ITryGet<TKey, TValue>, ITrySet<TKey, TValue>
+    public interface ITryGetSet<in TKey, TValue> : ITryGet<TKey, TValue>, ITrySet<TKey, TValue>
     {
     }
 }
