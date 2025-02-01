6e17072629a54e714b4e2fd1fe18635a2db45a58
miku-666 <74728189+NessieHax@users.noreply.github.com>
2025-02-01 11:57:41 +0100

    Add Editor.cs

---
+++ PCK-Studio/Internal/Editor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroFramework.Forms;
+using PckStudio.Interfaces;
+
+namespace PckStudio.Internal
+{
+    public abstract class Editor<T> : MetroForm where T : class
+    {
+        protected T EditorValue;
+        private readonly ISaveContext<T> SaveContext;
+
+        protected Editor(T value, ISaveContext<T> saveContext)
+        {
+            _ = value ?? throw new ArgumentNullException(nameof(value));
+            EditorValue = value;
+            SaveContext = saveContext;
+        }
+
+        protected void Save() => SaveContext.Save(EditorValue);
+    }
+}
