a031d40ca227306da93e83709f7f7a71d431136a
miku-666 <74728189+NessieHax@users.noreply.github.com>
2025-11-11 19:21:17 +0100

    Core - Add RootGameRule class

---
+++ PckStudio.Core/GameRule/RootGameRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OMI.Formats.GameRule;
+
+namespace PckStudio.Core.GameRule
+{
+    internal sealed class RootGameRule : AbstractGameRule
+    {
+        protected override GameRuleFile.GameRule GetGameRule() => new GameRuleFile.GameRule("__ROOT__"); //! name is irrelevant
+    }
+}
