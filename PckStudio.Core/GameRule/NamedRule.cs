66f59fe157cdc79cd8f6cb62e848b9c6a082f621
miku-666 <74728189+NessieHax@users.noreply.github.com>
2025-11-11 19:22:58 +0100

    Core - Add NamedRule class

---
+++ PckStudio.Core/GameRule/NamedRule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OMI.Formats.GameRule;
+
+namespace PckStudio.Core.GameRule
+{
+    internal sealed class NamedRule : AbstractGameRule
+    {
+        private readonly string _name;
+        private readonly GameRuleFile.GameRuleParameter[] _parameters;
+
+        public NamedRule(string name, params GameRuleFile.GameRuleParameter[] parameters)
+        {
+            _name = name;
+            _parameters = parameters;
+        }
+
+        protected override GameRuleFile.GameRule GetGameRule()
+        {
+            var rule = new GameRuleFile.GameRule(_name);
+            rule.AddParameters(_parameters);
+            return rule;
+        }
+    }
+}
