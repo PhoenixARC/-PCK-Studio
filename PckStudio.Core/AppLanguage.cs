07cda4b1779e55f1ea43070f74ac6d2b33b7c035
miku-666 <74728189+NessieHax@users.noreply.github.com>
2025-11-12 15:54:36 +0100

    Core - Add AppLanguage

---
+++ PckStudio.Core/AppLanguage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PckStudio.Core
+{
+    public enum AppLanguage : int
+    {
+        SystemDefault,
+        CzechCzechia,
+        Czechia,
+        Danish,
+        DenmarkDanish,
+        GermanAustria,
+        German,
+        GreekGreece,
+        Greece,
+        EnglishAustralia,
+        EnglishCanada,
+        English,
+        EnglishUnitedKingdom,
+        EnglishIreland,
+        EnglishNewZealand,
+        EnglishUnitedStatesOfAmerica,
+        SpanishSpain,
+        SpanishMexico,
+        FinnishFinland,
+        FrenchFrance,
+        FrenchCanada,
+        ItalianItaly,
+        JapaneseJapan,
+        KoreanSouthKorea,
+        Latin,
+        NorwegianNorway,
+        NorwegianBokmålNorway,
+        DutchNetherlands,
+        DutchBelgium,
+        PolishPoland,
+        PortugueseBrazil,
+        PortuguesePortugal,
+        RussianRussia,
+        SlovakSlovakia,
+        SwedishSweden,
+        TurkishTurkey,
+        ChineseChina,
+        ChineseHongKong,
+        ChineseSingapore,
+        ChineseTaiwan,
+    }
+}
