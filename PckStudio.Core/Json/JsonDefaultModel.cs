c9869171f57da402442b6b548d9eea98115c75d0
miku-666 <74728189+NessieHax@users.noreply.github.com>
2025-12-12 00:23:39 +0100

    Core(Box.cs) - Update class to be json serialisable

--- PckStudio.Core/Json/JsonDefaultModel.cs
+++ PckStudio.Core/Json/JsonDefaultModel.cs
@@ -25,24 +25,6 @@ namespace PckStudio.Core.Json
         public Vector3 Rotation { get; set; } = Vector3.Zero;
 
         [JsonProperty("boxes")]
-        public ModelDefaultBox[] Boxes { get; set; }
-    }
-
-    public class ModelDefaultBox
-    {
-        [JsonProperty("pos")]
-        public Vector3 Position { get; set; }
-
-        [JsonProperty("size")]
-        public Vector3 Size { get; set; }
-
-        [JsonProperty("uv")]
-        public Vector2 Uv { get; set; }
-
-        [JsonProperty("mirror")]
-        public bool Mirror { get; set; } = false;
-
-        [JsonProperty("inflate")]
-        public float Inflate { get; set; } = 0f;
+        public Box[] Boxes { get; set; }
     }
 }
