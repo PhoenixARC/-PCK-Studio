d9e4676b8c7382ebfd825dd34fbf2fff396727c4
miku-666 <74728189+NessieHax@users.noreply.github.com>
2025-12-20 12:11:29 +0100

    Core(ZipArchiveEntryExtensions) - Add 'GetImage' & 'GetDirectoryContent' extensions

--- PckStudio.Core/Extensions/ZipArchiveEntryExtensions.cs
+++ PckStudio.Core/Extensions/ZipArchiveEntryExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -16,6 +17,22 @@ namespace PckStudio.Core.Extensions
                 return string.Empty;
             using StreamReader reader = new StreamReader(entry.Open());
             return reader.ReadToEnd();
+        }
+
+        public static Image GetImage(this ZipArchiveEntry entry)
+        {
+            if (entry == null || (!entry.Name.EndsWith(".png") && !entry.Name.EndsWith(".jpg")))
+                return null;
+
+            using Stream stream = entry.Open();
+            Image image = Image.FromStream(stream);
+            stream.Dispose();
+            return image;
+        }
+
+        public static IEnumerable<ZipArchiveEntry> GetDirectoryContent(this ZipArchive zip, string path, string extention = "")
+        {
+            return zip.Entries.Where(e => e.FullName.StartsWith(path) && e.Name.EndsWith(extention) && !e.Name.EndsWith("/") && !e.Name.EndsWith("\\"));
         }
     }
 }
