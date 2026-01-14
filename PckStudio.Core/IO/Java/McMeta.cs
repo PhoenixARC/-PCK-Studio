fa9127a9cebc0143d149b0e4cbef66e6338978c9
miku-666 <74728189+NessieHax@users.noreply.github.com>
2026-01-14 17:27:23 +0100

    Core - Add McMeta.cs

---
+++ PckStudio.Core/IO/Java/McMeta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace PckStudio.Core.IO.Java
+{
+    public class McMeta : Dictionary<string, JObject>
+    {
+        private McMeta(Dictionary<string, JObject> type) : base(type)
+        {
+        }
+
+        public bool Contains(string name) => ContainsKey(name);
+
+        public static McMeta LoadMcMeta(string mcMetaJson)
+        {
+            JObject a = JObject.Parse(mcMetaJson);
+            Dictionary<string, JObject> types = new Dictionary<string, JObject>();
+            foreach (KeyValuePair<string, JToken> item in a)
+            {
+                types.Add(item.Key, item.Value.ToObject<JObject>());
+            }
+            return new McMeta(types);
+        }
+    }
+}
---
+++ PckStudio.Core/IO/Java/McPack.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using Newtonsoft.Json;
+
+namespace PckStudio.Core.IO.Java
+{
+    public struct McPack
+    {
+        [JsonIgnore]
+        public Image Icon;
+
+        [JsonProperty("pack_format")]
+        public int Format;
+
+        [JsonProperty("description")]
+        public string Description;
+    }
+}
+
