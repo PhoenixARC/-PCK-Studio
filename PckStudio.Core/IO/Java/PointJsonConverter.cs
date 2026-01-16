2a7885273abac860a79e0fa3b8ee61cb1a0a4cc3
miku-666 <74728189+NessieHax@users.noreply.github.com>
2026-01-16 15:33:21 +0100

    Core - Move PointJsonConverter class into PointJsonConverter.cs

---
+++ PckStudio.Core/IO/Java/PointJsonConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PckStudio.Core.IO.Java
+{
+    public class PointJsonConverter : JsonConverter<Point>
+    {
+        public override void WriteJson(JsonWriter writer, Point value, JsonSerializer serializer)
+        {
+            JObject jo = new JObject();
+            jo.Add("x", value.X);
+            jo.Add("y", value.Y);
+            jo.WriteTo(writer);
+        }
+
+        public override Point ReadJson(JsonReader reader, Type objectType, Point existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            JObject jo = JObject.Load(reader);
+            return new Point((int)(jo["x"] ?? 0), (int)(jo["y"] ?? 0));
+        }
+    }
+}
--- PckStudio.Core/IO/Java/SizeJsonConverter.cs
+++ PckStudio.Core/IO/Java/SizeJsonConverter.cs
@@ -21,21 +21,4 @@ namespace PckStudio.Core.IO.Java
             return new Size((int)(jo["width"] ?? 0), (int)(jo["height"] ?? 0));
         }
     }
-
-    public class PointJsonConverter : JsonConverter<Point>
-    {
-        public override void WriteJson(JsonWriter writer, Point value, JsonSerializer serializer)
-        {
-            JObject jo = new JObject();
-            jo.Add("x", value.X);
-            jo.Add("y", value.Y);
-            jo.WriteTo(writer);
-        }
-
-        public override Point ReadJson(JsonReader reader, Type objectType, Point existingValue, bool hasExistingValue, JsonSerializer serializer)
-        {
-            JObject jo = JObject.Load(reader);
-            return new Point((int)(jo["x"] ?? 0), (int)(jo["y"] ?? 0));
-        }
-    }
 }
